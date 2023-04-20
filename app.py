7736f2225d18159a34ae58417ad111948d0fcb25
cbauer2018 <ccbauer2018@gmail.com>
2023-04-20 12:03:38 -0600

    Adding the Flask Command to a new thread so ssh can exit

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import argparse
+import threading
+import signal
+import os
 from flask import Flask, request, jsonify
 import torch
 from transformers import pipeline
@@ -10,7 +13,6 @@ parser = argparse.ArgumentParser(description='Start the Flask app with the speci
 parser.add_argument('model_type', help='Model type to use')
 args = parser.parse_args()
 
-
 # Model type to model path in huggingface mapping
 MODEL_PATHS = {
     "Dolly V2 7B": "databricks/dolly-v2-7b",
@@ -21,6 +23,7 @@ MODEL_PATHS = {
 model_path = MODEL_PATHS.get(args.model_type)
 if not model_path:
     raise ValueError(f"Invalid model type: {args.model_type}")
+
 # Load the pipeline when the app starts
 instruct_pipeline = pipeline(
     model=model_path,
@@ -39,5 +42,20 @@ def inference():
     else:
         return jsonify({"error": "No prompt provided"}), 400
 
-if __name__ == '__main__':
+# Define a function to start the Flask app
+def start_flask_app():
     app.run(host='0.0.0.0', port=5000)
+
+# Define a signal handler to put the main thread into the background
+def sig_handler(signum, frame):
+    os.setsid()
+
+# Register the signal handler
+signal.signal(signal.SIGUSR1, sig_handler)
+
+# Start the Flask app in a separate thread
+flask_thread = threading.Thread(target=start_flask_app)
+flask_thread.start()
+
+# Send the signal to put the main thread into the background
+os.kill(os.getpid(), signal.SIGUSR1)
