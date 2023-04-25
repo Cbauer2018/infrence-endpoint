a101468419e2e124c88b0140b8ba98ef2191cbd6
cbauer2018 <ccbauer2018@gmail.com>
2023-04-25 09:22:06 -0600

    Reverting back to original code. Testng gunicorn for running Flask

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 import argparse
 import os
 import subprocess
-from multiprocessing import Process, Value, Manager
 
 app = Flask(__name__)
 
@@ -16,49 +15,29 @@ model_path = args.model_path
 if not model_path:
     raise ValueError(f"Invalid model path: {args.model_path}")
 
-# Global variables to store the model loading status and the pipeline
-model_loaded = Value('b', False)
-manager = Manager()
-instruct_pipeline = manager.Namespace()
-
-def load_model():
-    global model_loaded, instruct_pipeline
-    # Load the pipeline
-    pipeline_instance = pipeline(
-        model=model_path,
-        torch_dtype=torch.bfloat16,
-        trust_remote_code=True,
-        device_map="auto"
-    )
-    instruct_pipeline.pipeline = pipeline_instance
-    with model_loaded.get_lock():
-        model_loaded.value = True
+# Load the pipeline
+instruct_pipeline = pipeline(
+    model=model_path,
+    torch_dtype=torch.bfloat16,
+    trust_remote_code=True,
+    device_map="auto"
+)
 
 @app.route('/status', methods=['GET'])
 def status():
-    if model_loaded.value:
-        return jsonify({"status": "Model is loaded and API is running."})
-    else:
-        return jsonify({"status": "Model is loading. Please wait."})
+    return jsonify({"status": "Model is loaded and API is running."})
 
 @app.route('/', methods=['POST'])
 def inference():
-    if not model_loaded.value:
-        return jsonify({"error": "Model is still loading. Please wait and try again."}), 503
-
     prompt = request.json.get('prompt')
 
     if prompt:
-        result = instruct_pipeline.pipeline(prompt)
+        result = instruct_pipeline(prompt)
         return jsonify({"result": result})
     else:
         return jsonify({"error": "No prompt provided"}), 400
 
 if __name__ == '__main__':
-    # Start the Flask API in a separate process
-    api_process = Process(target=app.run, kwargs={'host': '0.0.0.0', 'port': 5000})
-    api_process.start()
+    app.run(host='0.0.0.0', port=5000)
 
-    # Load the model in the main process
-    load_model()
 
